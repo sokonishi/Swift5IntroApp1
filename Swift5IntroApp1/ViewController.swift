80c542dc036b9bab448d37cf9e215d6a1accc9cd
SoKonishi <sokonishi315@gmail.com>
2020-04-09 18:37:21 +0800

    Initial Commit

---
+++ Swift5IntroApp1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5IntroApp1
+//
+//  Created by 小西壮 on 2020/04/09.
+//  Copyright © 2020 小西壮. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swift5IntroApp1Tests/Swift5IntroApp1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Swift5IntroApp1Tests.swift
+//  Swift5IntroApp1Tests
+//
+//  Created by 小西壮 on 2020/04/09.
+//  Copyright © 2020 小西壮. All rights reserved.
+//
+
+import XCTest
+@testable import Swift5IntroApp1
+
+class Swift5IntroApp1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
